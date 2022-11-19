ebb6263bf08f2dd8d3f00d5e4fa95a1a092ac588
monk3yd <jegajardog@gmail.com>
2022-11-19 02:32:13 -0300

    Save old configs (before bspwm new rice)

---
+++ .config/bspwm_old/autostart.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+# Start polybar
+$HOME/.config/polybar/launch.sh &
+
+# Start compositor
+picom --config $HOME/.config/bspwm/picom.conf &
+
+# xfce4 dependencies
+run xfce4-power-manager &
+# run xfsettingsd &  # (appearance changed by lxappearance)
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+
+# update tray
+# run pamac-tray &
+
+# network tray
+run nm-applet &
+run blueman-applet &
+run insync start &
+
+sleep 15
+run volumeicon &
+
+run alacritty
+
+# conky -c $HOME/.config/bspwm/system-overview &
+# run variety &
+
+# numlockx on &
+
+
+#nitrogen --restore &
+#run caffeine &
+#run vivaldi-stable &
+#run firefox &
+#run thunar &
+#run dropbox &
+#run discord &
