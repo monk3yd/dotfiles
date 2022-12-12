0cdad54a176dbd554f66d6e9e1e116f687f0dc01
monk3yd <jegajardog@gmail.com>
2022-12-12 13:40:12 -0300

    Buffering reorder

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -28,8 +28,12 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-h>", ":BufferLineCyclePrev<CR>", opts)
+keymap("n", "<S-l>", ":BufferLineCycleNext<CR>", opts)
+
+-- Reorder buffers
+keymap("n", "<S-A-h>", ":BufferLineMovePrev<CR>", opts)
+keymap("n", "<S-A-l>", ":BufferLineMoveNext<CR>", opts)
 
 -- Close buffers
 keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
